1fd82dbe12669ab9306b9cf3d6943ccbbe59b962
ben <limhanyangb@gmail.com>
2018-02-07 14:15:29 -0600

    combined menu and huan code

--- TM4C Examples/MATLAB/simple_uart.m
+++ TM4C Examples/MATLAB/simple_uart.m
@@ -1,6 +1,5 @@
 % This is a basic interface to the TM4C controller through UART
 % 
-%
 % Configured for 115200 Baud Rate
 % Replace 'COM3' with the port that you see in Device Manager
 % 
@@ -8,12 +7,12 @@
 % terminates the line and returns the fscan function. 
 % 
 % Once done, fclose closes the port. If you see the port and cannot
-% connect to it, close and reopen MATLAB
+% connect to it, close and reopen MATLAB.
 %
 % R2R
 % Written By: Benjamen Lim & Huan
 % Date: 1 Feb 2018
-% 
+
 
 clear;clc;
 s = serial('COM5', 'BaudRate', 128000, 'DataBits', 8, 'StopBits', 1, 'Parity', 'none');
@@ -23,22 +22,8 @@ fopen(s);
 fwrite(s, 'b\n'); % Test by sending a char to the connected microcontroller
 %reply = fscanf(s)
 %reply = fread(s,4,'uchar')
-reply = fread(s, 4, 'float32')
+reply = fread(s, 4, 'float32') %read 4 bytes and convert to float
 fclose(s);
 %Afloat = typecast(reply , 'single') % I added it
 command = 1;
 
-
-% while true 
-%     clc
-%     disp('R2R Arm Interface')
-%     disp('-----------------')
-%     disp('System online.')
-%     disp('You last entered: ')
-%     disp(x);
-%     x = input('Do not enter k: ','s');
-%     if x == 'k'
-%         break;
-%     end
-%     
-% end
