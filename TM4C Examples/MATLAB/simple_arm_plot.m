ae5af82c8386a54b698b460cf94839efea8d7876
HuanWeng <huanweng2015@u.northwestern.edu>
2018-02-16 09:16:45 -0600

    Update: Simulation works
    Need to maximize the interface window.

--- TM4C Examples/MATLAB/simple_arm_plot.m
+++ TM4C Examples/MATLAB/simple_arm_plot.m
@@ -4,15 +4,13 @@ function simple_arm_plot(fig, jointangles)
 %handle and a array of joint angles.
 
 %***Choose the figure.
-%figure(fig)
-%figure
-hold on
-axis equal
-axis([-2.5, 2.5, -2.5, 2.5])
-axis off
+hold(fig, 'on')
+axis(fig, 'equal')
+axis(fig, [-2.5, 2.5, -2.5, 2.5])
+axis(fig, 'off')
 
 %***Plot background.
-plot(0, 0, 'Marker', '.', 'MarkerSize', 30, 'MarkerEdgeColor', 'k')
+plot(fig, 0, 0, 'Marker', '.', 'MarkerSize', 30, 'MarkerEdgeColor', 'k')
 %For the convenience of refer 'line' function, the x and y values of points
 %at the beginning and end of lines are devided into two matrices. Each
 %column of each matrix represents the x or y values of two points in a
@@ -21,8 +19,8 @@ pointsx = [0, -0.1, -0.1, -0.05,   0, -0.125, -0.075, -0.025, 0.025;
            0,  0.1, -0.05,    0, 0.05, -0.075, -0.025,  0.025, 0.075];
 pointsy = [   0, -0.1, -0.15, -0.15, -0.15, -0.15, -0.15, -0.15, -0.15; 
            -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1];            
-line(pointsx, pointsy, 'Color', 'k', 'LineWidth', 3);
-line([0, 1], [0, 0], 'LineStyle', '--', 'Color', 'k', 'LineWidth', 3)
+line(fig, pointsx, pointsy, 'Color', 'k', 'LineWidth', 3);
+line(fig, [0, 1], [0, 0], 'LineStyle', '--', 'Color', 'k', 'LineWidth', 3)
 
 %%
 
@@ -40,10 +38,10 @@ while i < size(jointangles, 1)
         t = temp(6);
         i = i + 1;
         delete(P);
-        P = [line([0, cos(jointangles(i, 1))], [0, sin(jointangles(i, 1))], 'Color', 'k', 'LineWidth', 3);
-             line([cos(jointangles(i, 1)), cos(jointangles(i, 1)) + cos(jointangles(i, 1) + jointangles(i, 2))], [sin(jointangles(i, 1)), sin(jointangles(i, 1)) + sin(jointangles(i, 1) + jointangles(i, 2))], 'Color', 'k', 'LineWidth', 3);
-             line([cos(jointangles(i, 1)), 2 * cos(jointangles(i, 1))], [sin(jointangles(i, 1)), 2 * sin(jointangles(i, 1))], 'LineStyle', '--', 'Color', 'k', 'LineWidth', 3);
-             plot(cos(jointangles(i, 1)), sin(jointangles(i, 1)), 'Marker', '.', 'MarkerSize', 30, 'MarkerEdgeColor', 'k')]; 
+        P = [line(fig, [0, cos(jointangles(i, 1))], [0, sin(jointangles(i, 1))], 'Color', 'k', 'LineWidth', 3);
+             line(fig, [cos(jointangles(i, 1)), cos(jointangles(i, 1)) + cos(jointangles(i, 1) + jointangles(i, 2))], [sin(jointangles(i, 1)), sin(jointangles(i, 1)) + sin(jointangles(i, 1) + jointangles(i, 2))], 'Color', 'k', 'LineWidth', 3);
+             line(fig, [cos(jointangles(i, 1)), 2 * cos(jointangles(i, 1))], [sin(jointangles(i, 1)), 2 * sin(jointangles(i, 1))], 'LineStyle', '--', 'Color', 'k', 'LineWidth', 3);
+             plot(fig, cos(jointangles(i, 1)), sin(jointangles(i, 1)), 'Marker', '.', 'MarkerSize', 30, 'MarkerEdgeColor', 'k')]; 
          drawnow
     end
 end
