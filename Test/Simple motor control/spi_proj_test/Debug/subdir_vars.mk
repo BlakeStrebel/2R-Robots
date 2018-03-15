3f44422028b4968d60e12d5fe0868d0969593ba0
ben <limhanyangb@gmail.com>
2018-03-14 21:42:32 -0500

    Simple motor control with Tiva

---
+++ Test/Simple motor control/spi_proj_test/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1294ncpdt.cmd 
+
+C_SRCS += \
+../spi_test.c \
+../tm4c1294ncpdt_startup_ccs.c 
+
+C_DEPS += \
+./spi_test.d \
+./tm4c1294ncpdt_startup_ccs.d 
+
+OBJS += \
+./spi_test.obj \
+./tm4c1294ncpdt_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"spi_test.obj" \
+"tm4c1294ncpdt_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"spi_test.d" \
+"tm4c1294ncpdt_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../spi_test.c" \
+"../tm4c1294ncpdt_startup_ccs.c" 
+
+
