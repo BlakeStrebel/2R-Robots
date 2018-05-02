3d9dfa0c9dc3c09b77cb265587fd355451a8cdc9
HuanWeng <huanweng2015@u.northwestern.edu>
2018-05-01 23:29:44 -0500

    Buy fix

--- MidtermCode/MATLAB/inverseKinematics2R.m
+++ MidtermCode/MATLAB/inverseKinematics2R.m
@@ -5,9 +5,9 @@ L1 = 1;
 L2 = 1;
 n = size(xytraj, 2);
 theta = zeros(2, n);
-l = norm(xytraj(:, i));
 
 for i = 1: n
+    l = norm(xytraj(:, i));
     theta(2, i) = pi - acos((L1^2 + L2^2 - l^2 ) / 2 / L1 / L2);
     theta(1, i) = atan(xytraj(2, i) / xytraj(1, i)) - acos((L1^2 + l^2 - L2^2) / 2 / L1 / l);
 end
