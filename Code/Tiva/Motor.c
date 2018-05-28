1ed9cbf7445b9403c34823ed3d4711c99dc129c8
ben <limhanyangb@gmail.com>
2018-05-28 14:38:44 -0500

    fixed broken code

--- Code/Tiva/Motor.c
+++ Code/Tiva/Motor.c
@@ -26,7 +26,7 @@ void motorSafetyCheck(){
         shutdownNow(); // shuts off the motor and turns on the brakes
         error_state = MOTOR_SPINNING_TOO_FAST; // set the error message
     }
-    if (tempRead1()>TEMP_LIMIT||tempRead2()>TEMP_LIMIT||){
+    if (tempRead1()>TEMP_LIMIT||tempRead2()>TEMP_LIMIT){
       shudownNow();// shuts off the motor and turns on the brakes
       error_state = MOTOR_TOO_HOT;// set the error message
     }
